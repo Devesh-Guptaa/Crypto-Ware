eaafb628deeed7a4e8e4fdd98b8b87f89e1a1da1
Devesh Gupta <deveshgupta231@gmail.com>
2022-06-10 15:28:56 +0530

    First Commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_Le4UtDcgkd3G-XHMSNZEOgFAHygA6zc",
+    authDomain: "crypto-ware-86010.firebaseapp.com",
+    projectId: "crypto-ware-86010",
+    storageBucket: "crypto-ware-86010.appspot.com",
+    messagingSenderId: "426939433722",
+    appId: "1:426939433722:web:83d168a2c498e8d8a6ff43"
+};
+
+export default firebaseConfig;
